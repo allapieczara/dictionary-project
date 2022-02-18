cecdc11e7c9697a6c07e66f2c84a7d881dae77a9
Alla <alla.pieczara@gmail.com>
2022-02-18 10:28:08 +0000

    Added dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,22 +1,36 @@
 import React, {useState} from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
-let [keyword, setKeyword] = useState("");
+  let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response){
+      console.log(response);
+  }
 
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
-  function handleKeywordChange(event){
-     console.log(event.target.value);
-     setKeyword(event.target.value);
+  function handleKeywordChange(event) {
+    console.log(event.target.value);
+    setKeyword(event.target.value);
   }
+
+  //https://api.dictionaryapi.dev/api/v2/entries/en/teal
 
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <input type="search" autoFocus={true} onChange={handleKeywordChange}></input>
+        <input
+          type="search"
+          autoFocus={true}
+          onChange={handleKeywordChange}
+        ></input>
       </form>
     </div>
   );
