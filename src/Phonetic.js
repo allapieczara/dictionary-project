95b5c1c5c6c3cb19f38d258f60f187b32965e06b
Alla <alla.pieczara@gmail.com>
2022-02-25 13:23:50 +0000

    corrected deploy error

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,9 +3,11 @@ import React from "react";
 export default function Phonetic(props) {
     console.log(props.phonetic);
     return (
-        <div className="Phonetic">
-            <a href={props.phonetic.audio} target="_blank">Listen</a>
-            {props.phonetic.text}
-        </div>
-    )
+      <div className="Phonetic">
+        <a href={props.phonetic.audio} rel="noreferrer"  target="_blank">
+          Listen
+        </a>
+        {props.phonetic.text}
+      </div>
+    );
 }
