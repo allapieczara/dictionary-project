d69073ac5295e0884254a200bd7f6f658b7eaf91
Alla <alla.pieczara@gmail.com>
2022-02-26 12:52:47 +0000

    Added styling to footer

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ export default function App() {
             rel="noreferrer"
             target="_blank"
           >
+            {" "}
             Alla Pieczara
           </a>
           <br />
