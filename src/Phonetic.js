0a36336e9fe038e1366a7ec795a7c12450bc439c
Alla <alla.pieczara@gmail.com>
2022-02-28 11:52:24 +0000

    Corrected Phonetic

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,13 +2,18 @@ import React from "react";
 import "./Phonetic.css";
 
 export default function Phonetic(props) {
-  
-  return (
-    <div className="Phonetic">
-      <a href={props.phonetic.audio} rel="noreferrer" target="_blank">
-        Listen
-      </a>
-      <span className="text">{props.phonetic.text}</span>
-    </div>
-  );
+  console.log(props.phonetic);
+
+  if (!props.phonetic.audio == "") {
+    return (
+      <div className="Phonetic">
+        <a href={props.phonetic.audio} rel="noreferrer" target="_blank">
+          Listen
+        </a>
+        <span className="text">{props.phonetic.text}</span>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
