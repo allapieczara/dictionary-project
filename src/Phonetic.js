9f5deb539a4db4788953ab68500aeb40e8a64725
Alla <alla.pieczara@gmail.com>
2022-02-28 12:08:07 +0000

    Phonetic update

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -7,15 +7,15 @@ export default function Phonetic(props) {
   if (!props.phonetic.audio === "") {
     return (
       <div className="Phonetic">
+        <a href={props.phonetic.audio} rel="noreferrer" target="_blank">
+          Listen
+        </a>
         <span className="text">{props.phonetic.text}</span>
       </div>
     );
   } else {
     return (
       <div className="Phonetic">
-        <a href={props.phonetic.audio} rel="noreferrer" target="_blank">
-          Listen
-        </a>
         <span className="text">{props.phonetic.text}</span>
       </div>
     );
